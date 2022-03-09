041e946824b1816ee478a4a8f6426e44e3cdb0f2
jiraspion <100152305+jiraspion@users.noreply.github.com>
2022-03-09 09:08:18 -0300

    exemplo de extenção

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string nome = "testando o nome aqui";
+
+            Console.WriteLine(nome.Cortar(5));
+
+            DateTime dt = new DateTime(2022, 01, 01, 8, 10, 40);
+
+            Console.WriteLine(dt.DataCortada());
+            
+        }
+    }
+}
---
+++ extencao.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Globalization;
+
+namespace System
+{
+    static class extencao
+    {
+        public static string Cortar(this string thisObjeto, int count)
+        {
+            if(thisObjeto.Length <= count)
+            {
+                return thisObjeto;
+            }
+            else
+            {
+                return thisObjeto.Substring(0, count) + "...";
+            }
+        }
+
+        public static string DataCortada(this DateTime thisObjeto)
+        {
+            TimeSpan duration = DateTime.Now.Subtract(thisObjeto);
+
+            if (duration.TotalHours < 24.0)
+            {
+                return duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + " hours";
+            }
+            else
+            {
+                return duration.TotalDays.ToString("F1", CultureInfo.InvariantCulture) + " day";
+            }
+
+        }
+    }
+}
